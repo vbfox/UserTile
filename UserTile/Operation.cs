a96fac032c23601ef239d99a66089693d64162a4
Julien Roncaglia <julien@roncaglia.fr>
2011-10-17 23:15:23 +0200

    Add license text.

--- UserTile/Operation.cs
+++ UserTile/Operation.cs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2011, Roncaglia Julien <julien@roncaglia.fr>
+ * 
+ * This program is open source; you can redistribute it and/or modify it under the terms of the BSD 2-Clause license 
+ * as specified in COPYING.txt
+ */
 namespace BlackFox.UserTile
 {
     enum Operation
